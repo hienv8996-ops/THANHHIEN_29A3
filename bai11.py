af92cb2a11831725746d96338f56eb226e4411c3
Vũ Hiền <hienv8996@gmail.com>
2026-09-22 08:34:14 +0700

    Enhance quadratic equation solver logic
    Fix conditional structure and add handling for two real roots.

--- bai11.py
+++ bai11.py
@@ -8,6 +8,12 @@ else:
     delta= b*b-4*a*c
     if delta<0:
         print('phương trình vô nghiệm')
-    else:
+    elif:
         x=-b/(2*a)
         print('phương trình có nghiệm kép')
+    else:
+        x1=(-b + math.sqrt(delta))/(2*a)
+        x2=(-b - math.sqrt(delta))/(2*a)
+        print('phương trình có 2 nghiệm: ')
+        print('x1 = ',x1)
+        print('x2 = ',x2)
